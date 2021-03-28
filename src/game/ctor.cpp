9ee76ace7550c951860dececdf284a5f94c665bb
Christopher Boyle <chrismjboyle@gmail.com>
2021-03-28 14:56:54 +0100

    Tweak ctor

--- src/game/ctor.cpp
+++ src/game/ctor.cpp
@@ -5,7 +5,8 @@ Game::Game(double dt, double g, int w, int h)
   , h(h)
   , dt(dt)
   , g(g)
-  , time(0)
+  , irl_time(0)
+  , physics_time(0)
   , should_quit(false)
   , win(nullptr)
   , renderer(nullptr)
@@ -15,8 +16,10 @@ Game::Game(double dt, double g, int w, int h)
   po->add_geometry(new Line(-0.5, 0, -100, 100));
   this->objects.push_back(po);
 
-  po = new PhysicsObject({0, 10}, false);
+  po = new PhysicsObject({0, 4}, false);
   po->add_geometry(gen_circle(1));
   this->objects.push_back(po);
+
   this->render_init();
+  this->physics_init();
 }
