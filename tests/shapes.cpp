ee0179e061e6b2221e69e996d3116c2567cef619
Christopher Boyle <chrismjboyle@gmail.com>
2021-03-29 18:42:54 +0100

    Add some more line intersection tests

--- tests/shapes.cpp
+++ tests/shapes.cpp
@@ -1,3 +1,7 @@
+// Appease the almighty linter
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
+
 #include <armadillo>
 #include <gtest/gtest.h>
 
@@ -11,3 +15,50 @@ TEST(ShapeTest, Lines_Parallel) {
 
     EXPECT_FALSE(v);
 }
+
+TEST(ShapeTest, Lines_Parallel_Close) {
+    Line line_a(0, 0, -1, 1), line_b(0, 1e-2, -1, 1);
+
+    arma::vec2 norm;
+    bool v = line_a.intersects(line_b, norm);
+
+    EXPECT_FALSE(v);
+}
+
+TEST(ShapeTest, Lines_Parallel_VeryClose) {
+    Line line_a(0, 0, -1, 1), line_b(0, 6e-3, -1, 1);
+
+    arma::vec2 norm;
+    bool v = line_a.intersects(line_b, norm);
+
+    EXPECT_FALSE(v);
+}
+
+TEST(ShapeTest, Lines_Parallel_Touching) {
+    Line line_a(0, 0, -1, 1), line_b(0, 4.5e-3, -1, 1);
+
+    arma::vec2 norm;
+    bool v = line_a.intersects(line_b, norm);
+
+    EXPECT_TRUE(v);
+}
+
+TEST(ShapeTest, Lines_Intersecting_InRange) {
+    Line line_a(-0.5, 0, -1, 1), line_b(0.5, 0, -1, 1);
+
+    arma::vec2 norm;
+    bool v = line_a.intersects(line_b, norm);
+
+    EXPECT_TRUE(v);
+}
+
+TEST(ShapeTest, Lines_Intersecting_OutOfRange) {
+    Line line_a(0.125, 1, -1, 1), line_b(0.5, 0, -1, 1);
+
+    arma::vec2 norm;
+    bool v = line_a.intersects(line_b, norm);
+
+    EXPECT_FALSE(v);
+}
+
+#pragma clang diagnostic pop
