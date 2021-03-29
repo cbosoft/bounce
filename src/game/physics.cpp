5310f9eae7118bee8736f922b4ad0346c4e55b3d
Christopher Boyle <chrismjboyle@gmail.com>
2021-03-29 10:21:58 +0100

    Prep for material characteristics

--- src/game/physics.cpp
+++ src/game/physics.cpp
@@ -44,14 +44,22 @@ void Game::physics_timestep_objects()
           double n2 = arma::dot(norm, norm);
           double vn = arma::dot(v, norm);
           arma::vec2 vel_parallel_to_norm = (vn/n2)*norm;
+          // if (vel_parallel_to_norm[0]*v[0] < 0)
+          //   vel_parallel_to_norm *= -1.0;
           arma::vec2 vel_perpendicular_to_norm = v - vel_parallel_to_norm;
 
+          double e = 0.5, f=e;
+          vel_parallel_to_norm *= e; // energy lost to heat, sound
+          vel_perpendicular_to_norm *= f; //energy lost to friction
+
           arma::vec2 new_vel = vel_perpendicular_to_norm - vel_parallel_to_norm;
-          // std::cerr
-          //   << n2 << " "
-          //   << vn << " "
-          //   << "(" << new_vel[0] << ", " << new_vel[1] << ") "
-          //   << std::endl;
+          std::cerr
+            << n2 << " "
+            << vn << " "
+            << "(" << norm[0] << ", " << norm[1] << ") "
+            << "(" << vel_parallel_to_norm[0] << ", " << vel_parallel_to_norm[1] << ") "
+            << "(" << new_vel[0] << ", " << new_vel[1] << ") "
+            << std::endl;
           free_body->velocity = new_vel;
           free_body->new_position = free_body->velocity*dt + free_body->position;
         }
