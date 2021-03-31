91d58fa148e29f1b18ac5d921ae101276d25a18c
Chris Boyle <chrismjboyle@gmail.com>
2021-03-31 15:29:03 +0100

    Move object init out of game ctor

--- src/game/ctor.cpp
+++ src/game/ctor.cpp
@@ -7,18 +7,5 @@ Game::Game(int w, int h)
   , win(nullptr)
   , renderer(nullptr)
 {
-  // OBJECTS INIT
-  auto *po = new PhysicsObject({0, 10}, false);
-  po->add_geometry(gen_circle(1));
-  this->add_object(po);
-
-  po = new PhysicsObject({0, 0}, true);
-  po->add_geometry(new Line(-0.5, 0, -100, 100));
-  this->add_object(po);
-
-  po = new PhysicsObject({0, -15}, true);
-  po->add_geometry(new Line(1, 0, -100, 100));
-  this->add_object(po);
-
   this->render_init();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include "game/game.hpp"
 #include "physics/field/force_fields.hpp"
+#include "shapes/shapes.hpp"
 
 int main()
 {
   Game game;
   auto &pe = PhysicsEngine::engine();
   pe.add_field(new UnboundedLinearForceField(0, 0, 0, -10));
+
+  PhysicsObject *o = nullptr;
+
+  o = new PhysicsObject({0, 0}, true);
+  o->add_geometry(new Line(0.1, 0, -50, 50));
+  game.add_object(o);
+
+  o = new PhysicsObject({-50, -50}, true);
+  o->add_geometry(new Line(1e9, 0, 0, 1e-7));
+  game.add_object(o);
+
+  o = new PhysicsObject({ 50, -50}, true);
+  o->add_geometry(new Line(1e9, 0, 0, 1e-7));
+  game.add_object(o);
+
+  // ball
+  o = new PhysicsObject({0, 10}, false);
+  o->add_geometry(gen_circle(1.0));
+  game.add_object(o);
 
   game.run();
   return 0;
