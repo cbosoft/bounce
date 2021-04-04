650dcfcc77c607bdaf2a24cf87e8e478dde69c83
Christopher Boyle <chrismjboyle@gmail.com>
2021-04-04 12:27:02 +0100

    Init force and mass in ctor for PhysicsObject

--- src/physics/object/object.cpp
+++ src/physics/object/object.cpp
@@ -5,6 +5,9 @@ PhysicsObject::PhysicsObject(const arma::vec2 &position, bool fixed)
   : position(position)
   , new_position(position)
   , velocity({0, 0})
+  , force({0,0})
+  , mass(1.0)
+  , inv_mass(1.0)
   , _fixed(fixed)
 {
     this->set_mass(1.0);
@@ -15,6 +18,8 @@ PhysicsObject::PhysicsObject(const arma::vec2 &position, double mass)
   , new_position(position)
   , velocity({0, 0})
   , force({0, 0})
+  , mass(1.0)
+  , inv_mass(1.0)
   , _fixed(false)
 {
     this->set_mass(mass);
