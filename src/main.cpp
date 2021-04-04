0521bd0be79f9b107c98ca8d2afa9311460f1052
Christopher Boyle <chrismjboyle@gmail.com>
2021-04-04 13:33:37 +0100

    Add a third ball

--- src/main.cpp
+++ src/main.cpp
@@ -4,33 +4,48 @@
 
 int main()
 {
-  Game game;
-  auto &pe = PhysicsEngine::init_engine(5e-5, 1.0);
-  pe.add_field(new UnboundedLinearForceField(0, 0, 0, -10));
+    Game game;
+    auto &pe = PhysicsEngine::init_engine(1e-4, 1.0);
+    pe.add_field(new UnboundedLinearForceField(0, 0, 0, -10));
 
-  PhysicsObject *o = nullptr;
+    PhysicsObject *o = nullptr;
 
-  o = new PhysicsObject({0, 0}, true);
-  o->add_geometry(new Line(0.1, 0, -50, 50));
-  game.add_object(o);
+    // platform
+    o = new PhysicsObject({0, 0}, true);
+    o->add_geometry(new Line(1, 0, -5, 5));
+    game.add_object(o);
 
-  o = new PhysicsObject({-50, -5}, true);
-  o->add_geometry(Line::from_points(arma::vec2{0, 0}, arma::vec2{0, 50}));
-  game.add_object(o);
+    // walls
+    o = new PhysicsObject({-50, 0}, true);
+    o->add_geometry(Line::from_points(arma::vec2{0, 0}, arma::vec2{0, 50}));
+    game.add_object(o);
 
-  o = new PhysicsObject({ 50, 0}, true);
-  o->add_geometry(Line::from_points(arma::vec2{0, 0}, arma::vec2{0, 50}));
-  game.add_object(o);
+    o = new PhysicsObject({ 50, 0}, true);
+    o->add_geometry(Line::from_points(arma::vec2{0, 0}, arma::vec2{0, 50}));
+    game.add_object(o);
 
-  // ball
-  o = new PhysicsObject({0, 10}, false);
-  o->add_geometry(gen_circle(1.0));
-  game.add_object(o);
+    // floor
+    o = new PhysicsObject({-50, 0}, true);
+    o->add_geometry(Line::from_points(arma::vec2{0, 0}, arma::vec2{50, -25}));
+    game.add_object(o);
 
-  o = new PhysicsObject({0, 20}, false);
-  o->add_geometry(gen_circle(1.0));
-  game.add_object(o);
+    o = new PhysicsObject({ 50, 0}, true);
+    o->add_geometry(Line::from_points(arma::vec2{0, 0}, arma::vec2{-50, -25}));
+    game.add_object(o);
 
-  game.run();
-  return 0;
+    // balls
+    o = new PhysicsObject({0, 10}, false);
+    o->add_geometry(gen_circle(1.0));
+    game.add_object(o);
+
+    o = new PhysicsObject({1, 20}, false);
+    o->add_geometry(gen_circle(1.0));
+    game.add_object(o);
+
+    o = new PhysicsObject({-1, 30}, false);
+    o->add_geometry(gen_circle(1.0));
+    game.add_object(o);
+
+    game.run();
+    return 0;
 }
