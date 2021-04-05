598526f34b756124738b255908b1c0e9eb338e39
Christopher Boyle <chrismjboyle@gmail.com>
2021-04-05 17:26:10 +0100

    Added git version tracker to cmake

---
+++ src/version.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+
+// From: https://github.com/andrew-hardin/cmake-git-version-tracking
+
+class GitMetadata {
+public:
+    // Is the metadata populated? We may not have metadata if
+    // there wasn't a .git directory (e.g. downloaded source
+    // code without revision history).
+    static bool populated();
+
+    // Were there any uncommitted changes that won't be reflected
+    // in the CommitID?
+    static bool is_dirty();
+
+    static std::string author_name();
+    static std::string author_email();
+    static std::string commit_SHA1();
+    static std::string commit_date();
+    static std::string commit_subject();
+    static std::string commit_body();
+    static std::string describe();
+    static std::string branch();
+};
