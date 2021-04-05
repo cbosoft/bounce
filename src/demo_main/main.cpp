d68a227ad4a11162523ef42644f5390c92e165e9
Christopher Boyle <chrismjboyle@gmail.com>
2021-04-05 22:16:09 +0100

    Playing about with point force fields

--- src/demo_main/main.cpp
+++ src/demo_main/main.cpp
@@ -7,59 +7,70 @@ int main()
     std::cerr << GitMetadata::version_string() << std::endl;
     Game game;
     auto &pe = PhysicsEngine::init_engine(1e-3, 2.0);
-    pe.add_field(new UnboundedLinearForceField(0, 0, 0, -10));
 
     PhysicsObject *o = nullptr;
     Transform root;
 
     // platform
+    ForceField *f = new PointSourceForceField(-0.1);
     o = new PhysicsObject(&root, {0, 0}, true);
+    f->set_parent(o);
     game.add_object(o);
+    pe.add_field(f);
 
     auto cam = game.get_camera();
     cam.set_parent(o);
 
-    double wrad = 1000, off=45;
+    // double wrad = 1000, off=45;
     // walls
-    o = new PhysicsObject(&root, {-wrad-off, wrad}, true);
-    o->set_radius(wrad);
-    game.add_object(o);
+    // o = new PhysicsObject(&root, {-wrad-off, wrad}, true);
+    // o->set_radius(wrad);
+    // game.add_object(o);
 
-    o = new PhysicsObject(&root, { wrad+off, wrad}, true);
-    o->set_radius(wrad);
-    game.add_object(o);
-    o = new PhysicsObject(&root, { wrad+off, 0}, true);
-    o->set_radius(wrad);
-    game.add_object(o);
-    o = new PhysicsObject(&root, {-wrad-off, 0}, true);
-    o->set_radius(wrad);
-    game.add_object(o);
+    // o = new PhysicsObject(&root, { wrad+off, wrad}, true);
+    // o->set_radius(wrad);
+    // game.add_object(o);
+    // o = new PhysicsObject(&root, { wrad+off, 0}, true);
+    // o->set_radius(wrad);
+    // game.add_object(o);
+    // o = new PhysicsObject(&root, {-wrad-off, 0}, true);
+    // o->set_radius(wrad);
+    // game.add_object(o);
 
     // floor
-    o = new PhysicsObject(&root, {-wrad-off,-wrad-off}, true);
-    o->set_radius(wrad);
-    game.add_object(o);
+    // o = new PhysicsObject(&root, {-wrad-off,-wrad-off}, true);
+    // o->set_radius(wrad);
+    // game.add_object(o);
 
-    o = new PhysicsObject(&root, { 0,-wrad-off}, true);
-    o->set_radius(wrad);
-    game.add_object(o);
+    // o = new PhysicsObject(&root, { 0,-wrad-off}, true);
+    // o->set_radius(wrad);
+    // game.add_object(o);
 
-    o = new PhysicsObject(&root, { wrad+off,-wrad-off}, true);
-    o->set_radius(wrad);
-    game.add_object(o);
+    // o = new PhysicsObject(&root, { wrad+off,-wrad-off}, true);
+    // o->set_radius(wrad);
+    // game.add_object(o);
 
     // balls
-    o = new PhysicsObject(&root, {0, 10}, false, 1.0);
-    game.add_object(o);
+    // o = new PhysicsObject(&root, {0, 10}, false, 1.0);
+    // game.add_object(o);
 
+    f = new PointSourceForceField(-0.1);
     o = new PhysicsObject(&root, {1, 20}, false, 1.0);
+    o->set_velocity({-10, 0});
+    f->set_parent(o);
     game.add_object(o);
+    pe.add_field(f);
 
-    o = new PhysicsObject(&root, {-2, 30}, false, 1.0);
-    game.add_object(o);
+    // o = new PhysicsObject(&root, {-2, 30}, false, 1.0);
+    // game.add_object(o);
 
-    o = new PhysicsObject(&root, {4, 40}, false, 1.0);
+    f = new PointSourceForceField(-0.1);
+    o = new PhysicsObject(&root, {0, 10}, false, 1.0);
+    o->set_radius(0.5);
+    o->set_velocity({10, 0});
+    f->set_parent(o);
     game.add_object(o);
+    pe.add_field(f);
 
     game.run();
     return 0;
