2094064e759794bd39ac242cea706b61b89e2f09
Christopher Boyle <chrismjboyle@gmail.com>
2021-04-09 14:58:38 +0100

    Add wasd alt movement

--- src/common/input/keyboard/handle_input.cpp
+++ src/common/input/keyboard/handle_input.cpp
@@ -12,9 +12,13 @@ void KeyboardInputManager::handle_input()
         context->back();
     }
 
-    if (glfwGetKey(win, GLFW_KEY_LEFT) == GLFW_PRESS)  { context->left(); }
-    if (glfwGetKey(win, GLFW_KEY_RIGHT) == GLFW_PRESS) { context->right(); }
-    if (glfwGetKey(win, GLFW_KEY_UP) == GLFW_PRESS)    { context->up(); }
-    if (glfwGetKey(win, GLFW_KEY_DOWN) == GLFW_PRESS)  { context->down(); }
+#define KEYPRESSED(KEY) (glfwGetKey(win, KEY) == GLFW_PRESS)
+
+    if (KEYPRESSED(GLFW_KEY_UP)    || KEYPRESSED(GLFW_KEY_W)) { context->up(); }
+    if (KEYPRESSED(GLFW_KEY_LEFT)  || KEYPRESSED(GLFW_KEY_A)) { context->left(); }
+    if (KEYPRESSED(GLFW_KEY_DOWN)  || KEYPRESSED(GLFW_KEY_S)) { context->down(); }
+    if (KEYPRESSED(GLFW_KEY_RIGHT) || KEYPRESSED(GLFW_KEY_D)) { context->right(); }
+
+#undef KEYPRESSED
 
 }
