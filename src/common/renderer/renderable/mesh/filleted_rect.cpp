401cafba2485a9aea938a468810d3724863a8f4c
Christopher Boyle <chrismjboyle@gmail.com>
2021-04-13 10:41:43 +0100

    Scale renderables properly based on construction sizes

--- src/common/renderer/renderable/mesh/filleted_rect.cpp
+++ src/common/renderer/renderable/mesh/filleted_rect.cpp
@@ -2,20 +2,23 @@
 
 MeshRenderable *MeshRenderable::filleted_rectangle(double w, double h, double r)
 {
-    double hh = 1.0, hw = 1.0, aspect = w/h;
+    double hh = 1.0, hw = 1.0, aspect = w/h, scale = h;
     if (aspect > 1.0) {
         hh = 1./aspect;
-        r /= w;
+        scale = w;
     }
     else {
         hw = aspect;
-        r /= h;
     }
 
-    return new MeshRenderable({
+    r /= scale;
+
+    auto *rbl = new MeshRenderable({
         {-hw, -hh+r}, {-hw+r, -hh},
         {hw-r, -hh}, {hw, -hh+r},
         {hw, hh-r}, {hw-r, hh},
         {-hw+r, hh}, {-hw, hh-r}
     });
+    rbl->set_scale(scale);
+    return rbl;
 }
--- src/common/renderer/renderable/mesh/rectangle.cpp
+++ src/common/renderer/renderable/mesh/rectangle.cpp
@@ -2,13 +2,16 @@
 
 MeshRenderable * MeshRenderable::rectangle(double w, double h)
 {
-    double hh = 1.0, hw = 1.0, aspect = w/h;
+    double hh = 1.0, hw = 1.0, aspect = w/h, scale = h;
     if (aspect > 1.0) {
         hh = 1./aspect;
+        scale = w;
     }
     else {
         hw = aspect;
     }
 
-    return new MeshRenderable({ {-hw, -hh}, {-hw, hh}, {hw, hh}, {hw, -hh} });
+    auto *rbl = new MeshRenderable({ {-hw, -hh}, {-hw, hh}, {hw, hh}, {hw, -hh} });
+    rbl->set_scale(scale);
+    return rbl;
 }
