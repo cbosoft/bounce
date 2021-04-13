5971831f80829ec4744f09bcc9f958cfa2abe100
Christopher Boyle <chrismjboyle@gmail.com>
2021-04-13 10:40:40 +0100

    Add ordered dithering fragment shader

---
+++ resources/shaders/fragment/ordered_dithering.glsl
@@ -0,0 +1,36 @@
+#version 330 core
+
+in vec2 TexCoords;
+out vec4 color;
+
+uniform sampler2D screenTexture;
+// http://alex-charlton.com/posts/Dithering_on_the_GPU/
+const int imat[64] = int[](0,  32, 8,  40, 2,  34, 10, 42,
+48, 16, 56, 24, 50, 18, 58, 26,
+12, 44, 4,  36, 14, 46, 6,  38,
+60, 28, 52, 20, 62, 30, 54, 22,
+3,  35, 11, 43, 1,  33, 9,  41,
+51, 19, 59, 27, 49, 17, 57, 25,
+15, 47, 7,  39, 13, 45, 5,  37,
+63, 31, 55, 23, 61, 29, 53, 21);
+
+float index_value() {
+    const float sl = 8.f, sl2 = sl*sl;
+    int x = int(mod(gl_FragCoord.x, sl));
+    int y = int(mod(gl_FragCoord.y, sl));
+    return imat[(x + 4*y)]/sl2;
+}
+
+float dither(float gray) {
+    float closest_colour = (gray < 0.5) ? 0.f : 1.f;
+    float distance = abs(closest_colour - gray);
+    float d = index_value()+0.1;
+    float second_place = 1.f - closest_colour;
+    return (distance <= d) ? closest_colour : second_place;
+}
+
+void main() {
+    vec4 rgb = texture(screenTexture, TexCoords);
+    float gy = (rgb.x + rgb.y + rgb.z)/3.f;
+    color = vec4(vec3(dither(gy)), 1.f);
+}
