30fc41608db5b9d8409c9d54a1df2c183957693b
Christopher Boyle <chrismjboyle@gmail.com>
2021-04-27 09:08:32 +0100

    Add abstract even which fires after a set amount of in-game time

---
+++ src/engine/event/delayed/check.cpp
@@ -0,0 +1,9 @@
+#include "delayed.hpp"
+
+bool TimeDelayedEvent::check(Game *game)
+{
+    (void) game;
+    EV_TIME_PT_T now = EV_CLOCK_T::now();
+    long dt = std::chrono::duration_cast<std::chrono::milliseconds>(now - this->_from).count();
+    return (dt > this->_ms);
+}
---
+++ src/engine/event/delayed/delayed.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <chrono>
+#include "../event.hpp"
+
+typedef std::chrono::system_clock EV_CLOCK_T;
+typedef EV_CLOCK_T::time_point EV_TIME_PT_T;
+
+class TimeDelayedEvent : public Event
+{
+public:
+    TimeDelayedEvent(long ms);
+
+    bool check(Game *game) override;
+
+private:
+    long _ms;
+    EV_TIME_PT_T _from;
+};
