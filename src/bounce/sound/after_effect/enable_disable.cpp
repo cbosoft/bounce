7098ef9ae2bf0f3c22596d1d0bde6ffa8c389fd0
Christopher Boyle <chrismjboyle@gmail.com>
2021-05-15 16:34:34 +0100

    Remove debugging message

--- src/bounce/sound/after_effect/enable_disable.cpp
+++ src/bounce/sound/after_effect/enable_disable.cpp
@@ -1,10 +1,8 @@
-#include <iostream>
 #include <bounce/sound/after_effect/after_effect.hpp>
 #include <bounce/sound/manager/manager.hpp>
 
 void SoundAfterEffect::enable()
 {
-    std::cerr << "ENABLE" << std::endl;
     SoundManager::ref().enable_effect(this);
 }
 
