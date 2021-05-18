8624e6d48a27e4b90033b5ce7ab10ea583b40e45
Christopher Boyle <chrismjboyle@gmail.com>
2021-05-18 19:15:36 +0100

    Fix bug where collider rbl shows on object even if not set to show

--- src/bounce/object/renderables.cpp
+++ src/bounce/object/renderables.cpp
@@ -5,7 +5,10 @@ void Object::get_renderables(std::list<const Renderable *> &out) const
 {
     if (Game::ref().should_show_colliders() && this->_renderable_collider) {
         this->_renderable_collider->set_size({this->_shape.w, this->_shape.h});
-        out.push_back(this->_renderable_collider);
+        this->_renderable_collider->show();
+    }
+    else if (this->_renderable_collider) {
+        this->_renderable_collider->hide();
     }
     Transform::get_renderables(out);
 }
